54033e622af4d800aff365478a72cb4b6afe76b5
gnso <milesbrentpatterson@proton.me>
2025-11-07 18:37:16 -0800

    indexedDB hotfix: wagmiConfig and wagmiConnectors

--- packages/nextjs/services/web3/wagmiConnectors.tsx
+++ packages/nextjs/services/web3/wagmiConnectors.tsx
@@ -31,16 +31,23 @@ const wallets = [
 /**
  * wagmi connectors for the wagmi context
  */
-export const wagmiConnectors = connectorsForWallets(
-  [
-    {
-      groupName: "Supported Wallets",
-      wallets,
-    },
-  ],
+export const wagmiConnectors = () => {
+  // only create connector on client side to avoid SSR issues
+  // TODO: update when https://github.com/rainbow-me/rainbowkit/issues/2476 is resolved
+  if (typeof window === "undefined") {
+    return [];
+  }
 
-  {
-    appName: "scaffold-eth-2",
-    projectId: scaffoldConfig.walletConnectProjectId,
-  },
-);
+  return connectorsForWallets(
+    [
+      {
+        groupName: "Supported Wallets",
+        wallets,
+      },
+    ],
+    {
+      appName: 'scaffold-dot',
+      projectId: scaffoldConfig.walletConnectProjectId,
+    },
+  );
+};
